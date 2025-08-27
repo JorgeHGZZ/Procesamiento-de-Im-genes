fa3357fa83926caaf3bbca896e0d2a86b4543cb2
Jorge Gonzalez <humberto.gonzalezmrn@uanl.edu.mx>
2025-08-26 18:29:02 -0600

    Botones

--- scanner.js
+++ scanner.js
@@ -12,3 +12,17 @@ model.addEventListener('targetLost', () => {
     console.log("Target perdido");
     model.setAttribute('animation-mixer', 'clip: *; loop: none; timeScale: 0;');
 });
+
+
+const homebutton = document.querySelector('#homeBtn');
+const exitbutton = document.querySelector('#exitButton');
+const collectionbutton = document.querySelector('#collectionButton');
+
+
+homebutton.addEventListener('click', function() {
+    window.location.href = "index.html";
+});
+
+exitbutton.addEventListener('click', function() {
+    window.location.href = "index.html";
+});
